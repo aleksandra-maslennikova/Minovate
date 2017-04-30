ebac35a3d1a9a9f22796ff054494d0e756b9ec71
aleksandra-maslennikova <alex.maslennikova19@gmail.com>
2017-04-30 11:03:58 +0300

    finished footer, added slider for logos

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 // https://www.npmjs.com/package/gulp-include
-
-
+//=require jquery/dist/jquery.min.js
+//=require slick-carousel/slick/slick.min.js
 
 
 $(function() {
@@ -9,4 +9,44 @@ $(function() {
 		var target = $(this).attr('href');
 		$(target).toggleClass('collapse');
 	})
+
+	$('.logos-slider').slick({
+		dots: false,
+		arrows: false,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 5,
+		slidesToScroll: 5,
+		autoplay: true,
+		responsive: [
+		{
+			breakpoint: 1200,
+			settings: {
+				slidesToShow: 4,
+				slidesToScroll: 4,
+			}
+		},
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+			}
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+	]
+});
 });
